093aed8c41a19f1c302445455726df6cd8f3195f
unknown <sakintola351@gmail.com>
2023-02-09 15:19:25 +0100

    Worked on login and sign up page design with no functionality

---
+++ chatapp/src/pages/ChatPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const ChatPage = () => {
+    return (
+        <div style={{ height: "100vh" }} className="chatContainer">
+            <h3>Chat Section</h3>
+        </div>
+    );
+};
+
+export default ChatPage;
